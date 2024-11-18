28da9358b68e2bd5d2e6d1e8b632ea6910bafd8a
Ronnie250 <Ronniemelwani@gmail.com>
2024-11-18 19:05:03 +0000

    Add files via upload

--- Bumblebee.py
+++ Bumblebee.py
@@ -8,15 +8,22 @@ bee=Actor("bee")
 flower=Actor("flower")
 flower.pos=300, 250
 score=0
+timer=60
+game_over=False
 
 def draw():
     screen.blit("background", (0,0))
     bee.draw()
     flower.draw()
     screen.draw.text("Score= "+str(score),(460,25), fontsize=40)
+    if game_over:
+        screen.blit("background", (0,0))
+        screen.draw.text("Score= "+str(score),(175,325), fontsize=75)
+    else:
+        screen.draw.text(str(int(timer)), (300,20))
 
-def update():
-    global score
+def update(dt):
+    global score, timer
     if keyboard.left:
         bee.x=bee.x-2
     if keyboard.right:
@@ -29,5 +36,13 @@ def update():
         score = score+1
         flower.x=random.randint(20,580)
         flower.y=random.randint(20,480)
+    if not game_over:
+        timer=timer-dt
+
+def time_up():
+    global game_over
+    game_over=True
+
+clock.schedule(time_up, 60.0)
 
 pgzrun.go()
